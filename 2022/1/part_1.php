1d2bc5d83e340a2cc0f0237275d3fcae214c77e7
Jesper Ravn <jspr.ravn@gmail.com>
2022-12-01 12:21:25 +0100

    Add comments

--- 2022/1/part_1.php
+++ 2022/1/part_1.php
@@ -1,5 +1,6 @@
 <?php
 
+// Read the file line by line
 $handle = fopen("./input.txt", "r");
 
 $result = [];
@@ -10,15 +11,18 @@ if ($handle) {
 
     while (($line = fgets($handle)) !== false) {
 
+      // Incase of line explicitly containing 0
       if ($line == "0") {
         continue;
       }
 
+      // New line, representing a new "elf"
       if (intval($line) === 0) {
         $i++;
         continue;
       }
 
+      // "Calories" added to a specific "elf"
       $result[$i][] = intval($line);
         
     }
@@ -26,10 +30,12 @@ if ($handle) {
     fclose($handle);
 }
 
+// Calculate the total "calories" for each "elf"
 foreach ($result as $key => $value) {
   $result[$key] = array_sum($value);
 }
 
+// Find the "elf" with the most "calories"
 $biggest = max($result);
 
 var_dump($biggest);
--- 2022/1/part_2.php
+++ 2022/1/part_2.php
@@ -1,7 +1,10 @@
 <?php
 
+// Get the list of "calories" for each "elf"
 $result = include("./part_1.php");
 
+// Sort the list of "calories" for each "elf" in descending order
 rsort($result);
 
+// Get the first 3 "calories" from the list combined
 var_dump($result[0]+$result[1]+$result[2]);
