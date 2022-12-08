08aeb19dcb3a1692a48345168013d11e4da8ce8b
Jesper Ravn <jspr.ravn@gmail.com>
2022-12-08 10:00:21 +0100

    Day 6 - python

---
+++ 2022/6/part_2.py
@@ -0,0 +1,19 @@
+import collections
+
+f = open("2022/6/input.txt", "r")
+
+marker_to_be_processed = []
+start = 0
+end = 14
+
+for line in f:
+  chars = list(line.strip())
+  while len(chars) > start:
+    marker_to_be_processed = chars[start:end]
+    duplicates = [item for item, count in collections.Counter(marker_to_be_processed).items() if count > 1]
+    if len(duplicates) == 0:
+      break
+    start += 1
+    end += 1
+
+print('Marker: ', marker_to_be_processed, ' Start position: ', start, ' End position: ', end)
